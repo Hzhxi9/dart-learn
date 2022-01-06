050e708b5e73d09dbe4b9651884c67ddded555c8
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2022-01-06 19:22:51 +0800

    feat: class

--- index.dart
+++ index.dart
@@ -42,11 +42,78 @@ void main() {
     print(year);
   }
 
-  /**函数 */
+  /**
+   * 函数
+   * 每个函数的参数以及返回值都指定类型
+   **/
   int fib(int n) {
     if (n == 0 || n == 1) return n;
     return fib(n - 1) + fib(n - 2);
   }
 
   print(fib(10));
+
+  /**箭头函数 */
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  flybyObjects.where((element) => element.contains('turn')).forEach((print));
+
+  var voyager = Spacecraft('John', DateTime(2022, 9, 6));
+  voyager.describe();
+
+  var voyager3 = Spacecraft.unlaunched('Voyager III');
+  voyager3.describe();
+}
+
+/**类 */
+class Spacecraft {
+  String name;
+
+  DateTime? launchDate;
+
+  /**getter属性, 只读 */
+  int? get launchYear => launchDate?.year;
+
+  /**构造函数(Constructor) */
+  Spacecraft(this.name, this.launchDate) {}
+
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  /**方法 */
+  void describe() {
+    print('Spacecraft: $name');
+
+    var launchDate = this.launchDate;
+
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+
+      print("Launched: $launchYear($years years ago)");
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+/**继承类 */
+class Oribiter extends Spacecraft {
+  double altitude;
+
+  Oribiter(String name, DateTime launchDate, this.altitude)
+      : super(name, launchDate);
+}
+
+/**
+ * Mixin 是一种在多个类层次结构中重用代码的方法
+ **/
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+
+/**使用 Mixin 的方式继承这个类就可将该类中的功能添加给其它类 */
+class PilotedCraft extends Spacecraft with Piloted {
+  PilotedCraft(String name, DateTime launchDate) : super(name, launchDate);
 }
